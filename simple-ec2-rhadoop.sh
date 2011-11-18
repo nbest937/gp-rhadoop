79683692eb5a38b4ff4acacc53ca9a744bb26f3c
Neil Best <nbest@ci.uchicago.edu>
2011-11-18 14:23:24 -0600

    first commit before running as aws-climate

---
+++ simple-ec2-rhadoop.sh
@@ -0,0 +1,40 @@
+
+gp-instance-create -c simple-ec2-rhadoop.conf
+
+GPI=gpi-5a866587
+
+
+# Currently the stock recipe installs Hadoop 0.21.
+# We need 0.20 because of RHadoop dependencies.
+# The -x flag overwrites the recipes for the cluster
+# before they are applied
+
+gp-instance-start -x simple-ec2-rhadoop.files $GPI
+
+gp-instance-describe $GPI
+
+# Extract node names from gp-instance-describe output
+SERVER=ec2-50-17-174-55.compute-1.amazonaws.com
+LOGIN=ec2-107-22-92-255.compute-1.amazonaws.com
+
+
+# Set Unix password and push to nodes
+# Assumes ssh user matches cluster user
+
+ssh -t $SERVER 'sudo passwd $USER'
+ssh $SERVER sudo make -C /var/yp
+
+
+# edit simple-ec2-rstudio-login.sh to match AMI architecture
+
+###
+
+# . . . then install RStudio
+ssh $LOGIN 'bash -s' < simple-ec2-rstudio-login.sh
+
+# go into the AWS Management Console and open port 8787
+# under "Security Groups", "globus-provision", "Inbound"
+
+# RStudio is now running at
+
+echo "http://${LOGIN}:8787"
---
+++ simple-ec2-rstudio-login.sh
@@ -0,0 +1,6 @@
+sudo apt-get install -y libssl0.9.8 libapparmor1 apparmor-utils
+wget http://download2.rstudio.org/rstudio-server-0.94.110-i386.deb
+sudo dpkg -i rstudio-server-0.94.110-i386.deb
+# wget http://download2.rstudio.org/rstudio-server-0.94.110-amd64.deb
+# sudo dpkg -i rstudio-server-0.94.110-amd64.deb
+
